b14838829a5ddefae86a7cbf1a54a8e083371535
Jakub Dardzinski <kuba0221@gmail.com>
2024-04-15 13:15:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+from __future__ import annotations
+
+import sys
+import time
+
+from flask import Flask, Response, request
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/', defaults={'path': ''})
+@app.route('/<path:path>', methods=['GET', 'HEAD', 'POST', 'PUT', 'DELETE'])
+def catch_all(path):
+    time.sleep(2)
+    if request.is_json:
+        print(request.json, file=sys.stderr)
+    
+    return Response('You want path: %s' % path, status=200)
+
+if __name__ == '__main__':
+    app.run()
